e73bc38cb73a689e7c93fb27ba3527ec91a049fa
yuxiRen <rita.ren.yuxi@gmail.com>
2021-05-17 12:15:49 -0700

    listen click event, create a human

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
-
-    // Create Dino Constructor
-
+// Create Dino Constructor
+function Dino(species, weight, height, facts) {
+    this.species = species;
+    this.weight = weight;
+    this.height = height;
+    this.facts = facts;
+    this.image = "images/" + species.toLowerCase() + ".png";
+}
+Dino.prototype.addFact = function (fact) {
+    this.facts.push(fact);
+};
 
     // Create Dino Objects
-
+//let dino = new Dino();
 
     // Create Human Object
 
-    // Use IIFE to get human data from form
-
+// Use IIFE to get human data from form
+function getHuman() {
+    return (function () {
+        let name = getInputValue("name");
+        let heightFeet = parseFloat(getInputValue("feet"));
+        let heightInches = parseFloat(getInputValue("inches"));
+        let weight = parseFloat(getInputValue("weight"));
+        // 12 inch = 1 feet
+        return new Dino('human', weight, heightFeet * 12 + heightInches);
+    })();
+}
 
     // Create Dino Compare Method 1
     // NOTE: Weight in JSON file is in lbs, height in inches. 
@@ -30,3 +47,10 @@
 
 
 // On button click, prepare and display infographic
+document.getElementById("btn").addEventListener("click", function(){
+    const human = getHuman();
+    console.log(human);
+});
+function getInputValue(elementId) {
+    return document.getElementById(elementId).value;
+}
